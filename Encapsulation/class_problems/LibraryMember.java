488048c71bdf21a5720a10bc99a77663ba78b8a9
sd8644 <sd8644@srmist.edu.in>
2026-09-11 19:15:34 +0530

    PROBLEM 4 LibraryMember JavaBean, Chained Constructors & Security Answer

---
+++ Encapsulation/class_problems/LibraryMember.java
@@ -0,0 +1,52 @@
+public class LibraryMember {
+    private String membershipId;
+    private String name;
+    private boolean premiumMember;
+    private String securityAnswerHash;
+
+    public LibraryMember() {
+        this(null, null);
+    }
+
+    public LibraryMember(String name) {
+        this(null, name);
+    }
+
+    public LibraryMember(String membershipId, String name) {
+        this.membershipId = membershipId;
+        this.name = name;
+        this.premiumMember = false;
+    }
+
+    public String getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(String membershipId) {
+        if (this.membershipId == null) {
+            this.membershipId = membershipId;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPremiumMember() {
+        return premiumMember;
+    }
+
+    public void setPremiumMember(boolean premiumMember) {
+        this.premiumMember = premiumMember;
+    }
+
+    public void setSecurityAnswer(String answer) {
+        if (answer != null) {
+            this.securityAnswerHash = String.valueOf(answer.hashCode());
+        }
+    }
+}
