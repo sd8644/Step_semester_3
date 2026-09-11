cdd87b353957a00bc1adfed0d9685d09d3e760dd
sd8644 <sd8644@srmist.edu.in>
2026-09-11 19:12:47 +0530

    PROBLEM 3 Book Copy Circulation Guard

---
+++ Encapsulation/class_problems/BookInventory.java
@@ -0,0 +1,28 @@
+public class BookInventory {
+    private final int copiesTotal;
+    private int copiesAvailable;
+
+    public BookInventory(int copiesTotal) {
+        if (copiesTotal <= 0) {
+            throw new IllegalArgumentException("Invalid total copies");
+        }
+        this.copiesTotal = copiesTotal;
+        this.copiesAvailable = copiesTotal;
+    }
+
+    public void checkOut() {
+        if (copiesAvailable > 0) {
+            copiesAvailable--;
+        }
+    }
+
+    public void checkIn() {
+        if (copiesAvailable < copiesTotal) {
+            copiesAvailable++;
+        }
+    }
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+}
